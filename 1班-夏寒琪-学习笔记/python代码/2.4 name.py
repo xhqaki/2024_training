e66c3ba1d353e68bd77b21489c1749d762c04072
xhqaki <863362260@qq.com>
2024-06-02 23:40:00 -0700

    commit

---
+++ 1班-夏寒琪-学习笔记/python代码/2.4 name.py
@@ -0,0 +1,7 @@
+name="Hanqi Xia"
+name="Hanqi Xia".upper() #全大写
+print(name)
+name="Hanqi Xia".lower() #全小写
+print(name)
+name="Hanqi Xia".title() #首字母大写
+print(name)
---
+++ 1班-夏寒琪-学习笔记/python代码/2.5+2.6 名言.py
@@ -0,0 +1,6 @@
+#2.5 打印名人名言
+print("Napoleon once said,\"True talent is resolute aspiration.\"")
+#2.6 添加名字变量
+famous_person="Napoleon"
+message="\"True talent is resolute aspiration.\""
+print(f"{famous_person} once said,{message}")
