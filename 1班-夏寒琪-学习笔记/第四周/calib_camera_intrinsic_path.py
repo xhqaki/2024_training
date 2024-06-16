dccc96c463b4ab2679baf0b19fec345e23bfa36b
xhqaki <863362260@qq.com>
2024-06-16 20:41:04 +0800

    Update

--- 1班-夏寒琪-学习笔记/第四周/calib_camera_intrinsic_path.py
+++ 1班-夏寒琪-学习笔记/第四周/calib_camera_intrinsic_path.py
@@ -20,15 +20,12 @@ cursor.execute(query)
 records = cursor.fetchall()
 print(records)
 
+# 在数据表中创建新的一列
 cursor.execute("alter table data_info add column calib_camera_intrinsic text")
 for record in records:
-    # 假设record是一个元组，其中包含id和其他字段
-    # 你可以通过索引或列名来访问它们（如果cursor是DictionaryCursor）
-
-    # 示例：获取image_path字段的值（这里假设它是record的第一个元素）
-    calib_camera_intrinsic_path = record[5]  # 或者使用record['image_path']，如果cursor是DictionaryCursor
-
-    # 读取文件内容（这里假设是文本文件）
+    # 获取calib_camera_intrinsic_path 字段的值
+    calib_camera_intrinsic_path = record[5] 
+    # 读取文件内容
     try:
         with open( calib_camera_intrinsic_path, 'r') as file:
             file_content = file.read()
@@ -38,13 +35,10 @@ for record in records:
     except Exception as e:
         print(f"Error reading file: {e}")
         continue  # 跳过有错误的文件
-
-        # 更新data_info表，添加或更新file_content列
-        # 注意：这里假设你有一个名为file_content的列来存储内容
-
-    update_query = " UPDATE data_info  SET  calib_camera_intrinsic = %s  WHERE id = %s  -- 假设id是主键，用于标识每一行  "
+        # 更新data_info表，        
+    update_query = " update data_info  set  calib_camera_intrinsic = %s  where id = %s " # id是主键，用于标识每一行 
     try:
-        cursor.execute(update_query, (file_content, record[0]))  # 假设id是record的第二个元素
+        cursor.execute(update_query, (file_content, record[0]))  # id是record的第1个元素
     except Error as e:
         print(f"Error updating data in MySQL: {e}")
         connection.rollback()  # 如果有错误，回滚事务
